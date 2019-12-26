f45bebaa21502b6a2efd0683da41683f493af090
Sarah Condon <s.j.condon01@gmail.com>
2019-12-25 19:08:46 -0600

    Done.

--- app.rb
+++ app.rb
@@ -9,4 +9,15 @@ class App < Sinatra::Base
 	get "/info" do
 		erb :info
 	  end
+	
+	  get "/new" do
+		erb :new
+	  end
+		get '/goodbye' do
+		  "Goodbye World"
+		end
+		
+		get '/hello' do
+		"Hello World"
+		  end
 end
